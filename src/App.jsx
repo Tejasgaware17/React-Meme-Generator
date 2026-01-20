cbc935afbf8fe2aaa4f02e6a17c7c9341ce6e450
tejas <tejasgaware9403@gmail.com>
2026-01-20 12:25:51 +0530

    feat: add Main and Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-  return
-}
+import Header from "./components/Header";
+import Main from "./components/Main";
 
-export default App
+export default function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    );
+}
